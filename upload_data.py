b2941794122cb87f8115ff8ebd525e2429349e41
trdminh <doanminh750@gmail.com>
2025-03-10 22:27:23 +0700

    Upload to database

--- upload_data.py
+++ upload_data.py
@@ -2,25 +2,22 @@
 from crawl_data.metadata import access_data
 from crawl_data.convertdata import convert_property
 from crawl_data.crawl_data import get_html, get_script_data
-import asyncio
 import json
 
 from create.create_data import PropertyRentManagement
-async def main():
-    url = "https://www.domain.com.au/57-ironside-st-st-lucia-qld-4067-17440500"
-    html = await get_html([url])
-    result = get_script_data(html[0])
-    data = await access_data(result)
-    new_data = await convert_property(data)
-    propertyRent = PropertyRentManagement(new_data)
-    agency_id = await propertyRent.create_property_for_rent(new_data)
-
 import asyncio
+import csv
+async def main():
+    with open(".database/rent/st_lucia.csv", mode="r", newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        next(reader)
+        urls = [row[1] for row in reader]
+    htmls = await get_html(urls)
+    for html in htmls:
+        data = await get_script_data(html)
+        data = await access_data(data)
+        data = await convert_property(data)
+        property_rent = PropertyRentManagement(data)
+        await property_rent.create_property_for_rent(data)
 
-try:
-    loop = asyncio.get_running_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-loop.run_until_complete(main())
+asyncio.run(main())
